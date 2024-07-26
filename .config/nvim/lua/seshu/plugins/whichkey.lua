3006586e3c96cf7e0c57c064c241e5345ea19364
SeshuHomePc <seshu.basava70@gmail.com>
2024-07-26 22:55:05 +0200

    Updated which key

--- .config/nvim/lua/seshu/plugins/whichkey.lua
+++ .config/nvim/lua/seshu/plugins/whichkey.lua
@@ -1,43 +1,16 @@
-return {
+return { -- Useful plugin to show you pending keybinds.
 	"folke/which-key.nvim",
-	event = "VeryLazy",
-	init = function()
-		vim.o.timeout = true
-		vim.o.timeoutlen = 300
-	end,
-	opts = {
-		window = {
-			border = "single", -- none, single, double, shadow
-			position = "bottom", -- bottom, top
-			margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
-			padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
-			winblend = 0,
-		},
-		plugins = { spelling = true },
-		defaults = {
-			mode = { "n", "v" },
-			["<leader>e"] = { name = "[E]xplorer" },
-			["<leader>b"] = { name = "[B]uffers", c = { name = "[C]lose" } },
-			["<leader>g"] = { name = "[G]it" },
-			["<leader>x"] = { name = "[T]rouble" },
-			["<leader>f"] = { name = "[F]ind with Telescope" },
-			["<leader>s"] = { name = "[S]tyle options" },
-			["<leader>l"] = { name = "[L]azy" },
-		},
-	},
-	config = function(_, opts)
-		local wk = require("which-key")
-		wk.setup(opts)
-		wk.register(opts.defaults)
+	event = "VimEnter", -- Sets the loading event to 'VimEnter'
+	config = function() -- This is the function that runs, AFTER loading
+		-- Document existing key chains
+		require("which-key").add({
+			{ "<leader>f", group = "[F]ind with Telescope" },
+			{ "<leader>b", group = "[B]uffers" },
+			{ "<leader>g", group = "[G]it", icon = "" },
+			{ "<leader>x", group = "[T]rouble" },
+			{ "<leader>l", group = "[L]azy" },
+			{ "<leader>s", group = "[S]tyle options" },
+		})
+		require("which-key").setup()
 	end,
 }
--- set keymaps
--- local wk = require("which-key")
--- wk.register({ ["<leader>"] = {
--- 	b = {
--- 		name = "[B]uffer",
--- 		c = {
--- 			name = "[C]lose",
--- 		},
--- 	},
--- } })
