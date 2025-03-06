87768df737d11836e8a60a7fe7e4c3b5b3ca5d7a
SeshuHomePc <seshu.basava70@gmail.com>
2025-03-06 20:15:14 +0100

    Updated a few more things

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -13,8 +13,9 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
 	require("plugins.colorscheme"),
-	require("plugins.mini-icons"),
+	require("plugins.mini"),
 	require("plugins.which-key"),
+	require("plugins.lualine"),
 	require("plugins.gitsigns"),
 	require("plugins.telescope"),
 	require("plugins.lsp"),
@@ -25,52 +26,19 @@ require("lazy").setup({
 	require("plugins.vim-tmux-navigator"),
 	require("plugins.lint"),
 	require("plugins.neotree"),
+	require("plugins.indent-blankline"),
+	require("plugins.todo-comments"),
 	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
-
-	-- Highlight todo, notes, etc in comments
 	{
-		"folke/todo-comments.nvim",
-		event = "VimEnter",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		opts = { signs = false },
+		-- Powerful Git integration for Vim
+		"tpope/vim-fugitive",
 	},
-
-	{ -- Collection of various small independent plugins/modules
-		"echasnovski/mini.nvim",
-		config = function()
-			-- Better Around/Inside textobjects
-			--
-			-- Examples:
-			--  - va)  - [V]isually select [A]round [)]paren
-			--  - yinq - [Y]ank [I]nside [N]ext [Q]uote
-			--  - ci'  - [C]hange [I]nside [']quote
-			require("mini.ai").setup({ n_lines = 500 })
-
-			-- Add/delete/replace surroundings (brackets, quotes, etc.)
-			--
-			-- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-			-- - sd'   - [S]urround [D]elete [']quotes
-			-- - sr)'  - [S]urround [R]eplace [)] [']
-			require("mini.surround").setup()
-
-			-- Simple and easy statusline.
-			--  You could remove this setup call if you don't like it,
-			--  and try some other statusline plugin
-			local statusline = require("mini.statusline")
-			-- set use_icons to true if you have a Nerd Font
-			statusline.setup({ use_icons = vim.g.have_nerd_font })
-
-			-- You can configure sections in the statusline by overriding their
-			-- default behavior. For example, here we set the section for
-			-- cursor location to LINE:COLUMN
-			---@diagnostic disable-next-line: duplicate-set-field
-			statusline.section_location = function()
-				return "%2l:%-2v"
-			end
-
-			-- ... and there is more!
-			--  Check out: https://github.com/echasnovski/mini.nvim
-		end,
+	{
+		-- Autoclose parentheses, brackets, quotes, etc.
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+		opts = {},
 	},
 
 	-- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
@@ -122,11 +90,8 @@ require("lazy").setup({
 
 	-- The line beneath this is called `modeline`. See `:help modeline`
 	-- vim: ts=2 sts=2 sw=2 et
-	-- require("plugins.neotree"),
 	-- require("plugins.mini-animate"),
 	-- require("plugins.bufferline"),
-	-- require("plugins.lualine"),
 	-- require("plugins.todo-comments"),
-	-- require("plugins.indent-blankline"),
 	-- require("plugins.nvim-notify"),
 })
--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -5,16 +5,17 @@ return {
 			"mode",
 			fmt = function(str)
 				return " " .. str
-				-- return ' ' .. str:sub(1, 1) -- displays only the first character of the mode
 			end,
 		}
-		local lazy_status = require("lazy.status") -- to configure lazy pending updates count
+
+		local lazy_status = require("lazy.status")
 
 		local filename = {
 			"filename",
-			file_status = true, -- displays file status (readonly status, modified status)
-			path = 0, -- 0 = just filename, 1 = relative path, 2 = absolute path
+			file_status = true,
+			path = 0,
 		}
+
 		local hide_in_width = function()
 			return vim.fn.winwidth(0) > 100
 		end
@@ -27,23 +28,18 @@ return {
 			colored = true,
 			update_in_insert = true,
 			always_visible = false,
-			-- cond = hide_in_width,
 		}
 
 		local diff = {
 			"diff",
-			colored = false,
-			symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
-			-- cond = hide_in_width,
+			symbols = { added = "+", modified = "~", removed = "-" },
 		}
 
+		-- Setup Lualine
 		require("lualine").setup({
 			options = {
 				icons_enabled = true,
-				-- theme = 'tokyonight', -- Set theme based on environment variable
-				-- Some useful glyphs:
-				-- https://www.nerdfonts.com/cheat-sheet
-				--        
+				theme = "tokyonight",
 				component_separators = { left = "", right = "" },
 				section_separators = { left = "", right = "" },
 				disabled_filetypes = { "alpha", "neo-tree" },
@@ -55,10 +51,14 @@ return {
 				lualine_c = { filename },
 				lualine_x = {
 					diagnostics,
-					{ "encoding", cond = hide_in_width },
 					{ "filetype", cond = hide_in_width },
 				},
 				lualine_y = {
+					{
+						lazy_status.updates,
+						cond = lazy_status.has_updates,
+						color = { fg = "#ff9e64" },
+					},
 					{
 						function()
 							return " " .. os.date("%R")
@@ -66,27 +66,23 @@ return {
 					},
 				},
 				lualine_z = {
-					{ "progress" },
 					{ "location" },
-					-- { filename },
+					function()
+						local current_line = vim.fn.line(".")
+						local total_lines = vim.fn.line("$")
+						local columns = 15
+						local position = math.floor((current_line / total_lines) * columns)
+						local line = ""
+						for i = 0, columns do
+							if i == position then
+								line = line .. "⬥"
+							else
+								line = line .. "─"
+							end
+						end
+						return line
+					end,
 				},
-				--     function()
-				--       local current_line = vim.fn.line('.')
-				--       local total_lines = vim.fn.line('$')
-				--       local columns = 15  -- Total length of the "track"
-				--       local position = math.floor((current_line / total_lines) * columns)
-				--       local line = ""
-				--       for i = 0, columns do
-				--         if i == position then
-				--           line = line .. '⬥'  -- Diamond symbol (You can replace with ◇ or similar)
-				--         else
-				--           line = line .. '─'
-				--         end
-				--       end
-				--       return line
-				--     end,
-				--   }
-				-- },
 			},
 			inactive_sections = {
 				lualine_a = {},
@@ -99,5 +95,38 @@ return {
 			tabline = {},
 			extensions = { "fugitive" },
 		})
+
+		-- === Force Transparent Background for Lualine ===
+		local function make_lualine_transparent()
+			local sections = { "c", "x" } -- { "a", "b", "c", "x", "y", "z" }
+			local modes = { "normal", "insert", "visual", "replace", "command", "inactive" }
+
+			for _, hl_mode in ipairs(modes) do
+				for _, section in ipairs(sections) do
+					local hl_name = "lualine_" .. section .. "_" .. hl_mode
+					local ok, hl = pcall(vim.api.nvim_get_hl, 0, { name = hl_name })
+
+					if ok then
+						vim.api.nvim_set_hl(0, hl_name, {
+							fg = hl.fg,
+							bg = "none", -- Transparent
+							sp = hl.sp,
+							underline = hl.underline,
+							bold = hl.bold,
+							italic = hl.italic,
+						})
+					end
+				end
+			end
+		end
+
+		-- Run this after every colorscheme change to ensure lualine gets reset
+		vim.api.nvim_create_autocmd("ColorScheme", {
+			pattern = "*",
+			callback = make_lualine_transparent,
+		})
+
+		-- Apply transparency immediately (for first launch)
+		make_lualine_transparent()
 	end,
 }
