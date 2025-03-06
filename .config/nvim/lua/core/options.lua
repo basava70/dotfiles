e94d9dfbeea153459ed97a58ea8854d364d5ecb8
SeshuHomePc <seshu.basava70@gmail.com>
2025-03-06 17:20:54 +0100

    Fresh kickstart nvim

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -1,48 +1,147 @@
-vim.wo.number = true -- Make line numbers default (default: false)
-vim.o.relativenumber = true -- Set relative numbered lines (default: false)
-vim.o.clipboard = "unnamedplus" -- Sync clipboard between OS and Neovim. (default: '')
-vim.o.wrap = true -- Display lines as one long line (default: true)
-vim.o.linebreak = true -- Companion to wrap, don't split words (default: false)
-vim.o.mouse = "a" -- Enable mouse mode (default: '')
-vim.o.autoindent = true -- Copy indent from current line when starting new one (default: true)
-vim.o.ignorecase = true -- Case-insensitive searching UNLESS \C or capital in search (default: false)
-vim.o.smartcase = true -- Smart case (default: false)
-vim.o.shiftwidth = 4 -- The number of spaces inserted for each indentation (default: 8)
-vim.o.tabstop = 4 -- Insert n spaces for a tab (default: 8)
-vim.o.softtabstop = 4 -- Number of spaces that a tab counts for while performing editing operations (default: 0)
-vim.o.expandtab = true -- Convert tabs to spaces (default: false)
-vim.o.scrolloff = 4 -- Minimal number of screen lines to keep above and below the cursor (default: 0)
-vim.o.sidescrolloff = 8 -- Minimal number of screen columns either side of cursor if wrap is `false` (default: 0)
-vim.o.cursorline = true -- Highlight the current line (default: false)
-vim.o.splitbelow = true -- Force all horizontal splits to go below current window (default: false)
-vim.o.splitright = true -- Force all vertical splits to go to the right of current window (default: false)
-vim.o.hlsearch = true -- Set highlight on search (default: true)
-vim.o.showmode = false -- We don't need to see things like -- INSERT -- anymore (default: true)
-vim.opt.termguicolors = true -- Set termguicolors to enable highlight groups (default: false)
-vim.o.whichwrap = "bs<>[]hl" -- Which "horizontal" keys are allowed to travel to prev/next line (default: 'b,s')
-vim.o.numberwidth = 4 -- Set number column width to 2 {default 4} (default: 4)
-vim.o.swapfile = false -- Creates a swapfile (default: true)
-vim.o.smartindent = true -- Make indenting smarter again (default: false)
-vim.o.showtabline = 2 -- Always show tabs (default: 1)
-vim.o.backspace = "indent,eol,start" -- Allow backspace on (default: 'indent,eol,start')
-vim.o.pumheight = 10 -- Pop up menu height (default: 0)
-vim.o.conceallevel = 0 -- So that `` is visible in markdown files (default: 1)
-vim.wo.signcolumn = "yes" -- Keep signcolumn on by default (default: 'auto')
-vim.o.fileencoding = "utf-8" -- The encoding written to a file (default: 'utf-8')
-vim.o.cmdheight = 1 -- More space in the Neovim command line for displaying messages (default: 1)
-vim.o.breakindent = true -- Enable break indent (default: false)
-vim.o.updatetime = 250 -- Decrease update time (default: 4000)
-vim.o.timeoutlen = 300 -- Time to wait for a mapped sequence to complete (in milliseconds) (default: 1000)
-vim.o.backup = false -- Creates a backup file (default: false)
-vim.o.writebackup = false -- If a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited (default: true)
-vim.o.undofile = true -- Save undo history (default: false)
-vim.o.completeopt = "menuone,noselect" -- Set completeopt to have a better completion experience (default: 'menu,preview')
-vim.opt.shortmess:append("c") -- Don't give |ins-completion-menu| messages (default: does not include 'c')
-vim.opt.iskeyword:append("-") -- Hyphenated words recognized by searches (default: does not include '-')
-vim.opt.formatoptions:remove({ "c", "r", "o" }) -- Don't insert the current comment leader automatically for auto-wrapping comments using 'textwidth', hitting <Enter> in insert mode, or hitting 'o' or 'O' in normal mode. (default: 'croql')
-vim.opt.runtimepath:remove("/usr/share/vim/vimfiles") -- Separate Vim plugins from Neovim in case Vim still in use (default: includes this path if Vim is installed)
--- Sets how neovim will display certain whitespace characters in the editor.
---  See `:help 'list'`
---  and `:help 'listchars'`
--- vim.opt.list = true
--- vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+-- Set <space> as the leader key
+-- This key is used as the main prefix for all your custom mappings
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Set to true if you have a Nerd Font installed and selected in your terminal
+vim.g.have_nerd_font = true
+
+-- [[ UI Options ]]
+
+-- Show absolute line numbers
+vim.opt.number = true
+
+-- Use relative numbers (helps with quickly jumping lines with `j`/`k`)
+vim.opt.relativenumber = true
+
+-- Always show the sign column (used by gitsigns, LSP diagnostics, etc.)
+vim.opt.signcolumn = "yes"
+
+-- Highlight the current line to make cursor position more visible
+vim.opt.cursorline = true
+
+-- Enable true color support for better themes
+vim.opt.termguicolors = true
+
+-- Set command line height (some plugins might want more space here)
+vim.opt.cmdheight = 1
+
+-- Always show the tabline (even if only one tab is open)
+vim.opt.showtabline = 2
+
+-- Don't show --INSERT--, --VISUAL-- etc., since your statusline likely handles this
+vim.opt.showmode = false
+
+-- [[ Clipboard Settings ]]
+
+-- Sync clipboard between OS and Neovim (works with `unnamedplus`)
+-- This is deferred slightly to improve startup time
+vim.schedule(function()
+	vim.opt.clipboard = "unnamedplus"
+end)
+
+-- [[ Indentation and Tab Settings ]]
+
+-- Use auto-indenting for new lines
+vim.opt.autoindent = true
+
+-- Smarter auto-indenting (respects file types better)
+vim.opt.smartindent = true
+
+-- Wrap indented lines visually
+vim.opt.breakindent = true
+
+-- Number of spaces to use for each step of (auto)indent
+vim.opt.shiftwidth = 4
+
+-- Number of spaces a tab counts for (in visual representation)
+vim.opt.tabstop = 4
+
+-- Number of spaces for a tab during editing (insertion and deletion)
+vim.opt.softtabstop = 4
+
+-- Convert actual tab characters to spaces (soft tabs)
+vim.opt.expandtab = true
+
+-- [[ Wrapping and Cursor Movement ]]
+
+-- Enable line wrapping
+vim.opt.wrap = true
+
+-- Don't break wrapped lines in the middle of words
+vim.opt.linebreak = true
+
+-- Allow cursor to move to next/prev line with <Left>/<Right>, etc.
+vim.opt.whichwrap = "bs<>[]hl"
+
+-- Minimal number of screen lines to keep above and below cursor
+vim.opt.scrolloff = 4
+
+-- Minimal number of columns to keep left/right of cursor when not wrapping
+vim.opt.sidescrolloff = 8
+
+-- [[ Search Settings ]]
+
+-- Case-insensitive searching unless search term has uppercase
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Highlight search matches (can toggle with `:nohlsearch`)
+vim.opt.hlsearch = true
+
+-- Preview substitution results live in a split
+vim.opt.inccommand = "split"
+
+-- [[ Backup and Undo ]]
+
+-- Disable swap file (can be annoying, especially with modern projects)
+vim.opt.swapfile = false
+
+-- Disable backup files (redundant with version control)
+vim.opt.backup = false
+vim.opt.writebackup = false
+
+-- Enable undo history across sessions
+vim.opt.undofile = true
+
+-- [[ Performance Tweaks ]]
+
+-- Faster updates for better responsiveness (especially with LSP, git, etc.)
+vim.opt.updatetime = 250
+
+-- Shorter wait for mapped key sequences
+vim.opt.timeoutlen = 300
+
+-- [[ File Settings ]]
+
+-- Always use UTF-8 encoding
+vim.opt.fileencoding = "utf-8"
+
+-- Show all content in markdown (no "conceal" hiding)
+vim.opt.conceallevel = 0
+
+-- [[ Completion Settings ]]
+
+-- Show menu even if only one completion option exists, and don't auto-select
+vim.opt.completeopt = "menuone,noselect"
+
+-- [[ Miscellaneous Options ]]
+
+-- Enable mouse support in all modes
+vim.opt.mouse = "a"
+
+-- Show special characters for tabs, trailing spaces, etc.
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+-- Suppress unnecessary completion messages
+vim.opt.shortmess:append("c")
+
+-- Treat words with dashes as single words (useful for CSS, etc.)
+vim.opt.iskeyword:append("-")
+
+-- Remove legacy vim runtime path (helps keep Neovim clean)
+vim.opt.runtimepath:remove("/usr/share/vim/vimfiles")
+
+-- Prevent comment continuation when hitting `o`/`O`
+vim.opt.formatoptions:remove({ "c", "r", "o" })
