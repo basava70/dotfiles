f426860df01e544b81a3747408418fcbd9a85fd0
SeshuHomePc <seshu.basava70@gmail.com>
2025-03-07 16:09:55 +0100

    Updated nvim-notify

--- .config/nvim/lua/plugins/nvim-notify.lua
+++ .config/nvim/lua/plugins/nvim-notify.lua
@@ -1,9 +1,33 @@
+-- return {
+-- 	"rcarriga/nvim-notify",
+-- 	opts = {
+-- 		stages = "fade",
+-- 		timeout = 3000,
+-- 		background_colour = "#000000",
+-- 	},
+-- 	init = function()
+-- 		vim.notify = require("notify") -- Set global notify after plugin loads
+-- 	end,
+-- }
 return {
 	"rcarriga/nvim-notify",
+	event = "VeryLazy",
+	cmd = "Notifications",
 	opts = {
-		stages = "fade",
-		timeout = 3000,
-		background_colour = "#000000",
+		-- background_colour = "#000",
+		fps = 90,
+		icons = {
+			DEBUG = "",
+			ERROR = "",
+			INFO = "",
+			TRACE = "",
+			WARN = "",
+		},
+		level = 2,
+		minimum_width = 50,
+		render = "default",
+		stages = "slide",
+		timeout = 2000,
 	},
 	init = function()
 		vim.notify = require("notify") -- Set global notify after plugin loads
