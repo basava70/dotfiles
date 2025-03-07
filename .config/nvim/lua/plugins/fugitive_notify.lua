47abd7012bd4de90a616a4aadd51759a9206153f
SeshuHomePc <seshu.basava70@gmail.com>
2025-03-07 16:06:13 +0100

    Added fugitive_notify to make sure fugitive notifications are seen in nvim notify

---
+++ .config/nvim/lua/plugins/fugitive_notify.lua
@@ -0,0 +1,23 @@
+local M = {}
+
+function M.setup()
+	vim.api.nvim_create_autocmd("FileType", {
+		pattern = "gitcommit",
+		callback = function()
+			vim.api.nvim_create_autocmd("BufWritePost", {
+				buffer = 0,
+				callback = function()
+					local commit_message = vim.fn.join(vim.fn.readfile(vim.fn.expand("%")), " ")
+
+					vim.notify(
+						"Committed with message:\n" .. commit_message,
+						vim.log.levels.INFO,
+						{ title = "Git Commit" }
+					)
+				end,
+			})
+		end,
+	})
+end
+
+return M
