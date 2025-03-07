39497865c5200241c5dc2f3031b80aaf82182d8c
SeshuHomePc <seshu.basava70@gmail.com>
2025-03-07 16:23:55 +0100

    Updated nvim-notify fugitve message.

--- .config/nvim/lua/plugins/fugitive_notify.lua
+++ .config/nvim/lua/plugins/fugitive_notify.lua
@@ -7,17 +7,14 @@ function M.setup()
 			vim.api.nvim_create_autocmd("BufWritePost", {
 				buffer = 0,
 				callback = function()
-					local commit_message = table.concat(vim.fn.readfile(vim.fn.expand("%")), "\n")
+					-- Short delay to let Fugitive fully commit and update the log
+					vim.defer_fn(function()
+						local git_summary = vim.fn.systemlist("git log -1 --pretty=oneline --stat")
 
-					-- Capture git's post-commit summary (like files changed, insertions, deletions)
-					local git_summary = vim.fn.systemlist("git log -1 --stat")
+						local summary_text = table.concat(git_summary, "\n")
 
-					local combined_message = "Committed with message:\n"
-						.. commit_message
-						.. "\n\n"
-						.. table.concat(git_summary, "\n")
-
-					vim.notify(combined_message, vim.log.levels.INFO, { title = "Git Commit" })
+						vim.notify(summary_text, vim.log.levels.INFO, { title = "Git Commit Summary" })
+					end, 100) -- 100ms to ensure commit completes before reading log
 				end,
 			})
 		end,
