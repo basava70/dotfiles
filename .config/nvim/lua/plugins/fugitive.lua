8973881bc58a3f5935396d97bf94090070c6419b
SeshuHomePc <seshu.basava70@gmail.com>
2025-03-07 15:44:48 +0100

    Added more Fugitive keymappings and created an autocommand to make "q" work in fugitive buffer

--- .config/nvim/lua/plugins/fugitive.lua
+++ .config/nvim/lua/plugins/fugitive.lua
@@ -1,13 +1,20 @@
 return {
 	"tpope/vim-fugitive",
 	config = function()
-		-- Git Status
 		vim.keymap.set("n", "<leader>gs", ":Git<CR>", { desc = "[G]it [S]tatus" })
-
-		-- Git Diffsplit for current file
-		vim.keymap.set("n", "<leader>gd", ":Gdiffsplit<CR>", { desc = "[G]it [D]iff" })
-
-		-- Git Blame for current file
+		vim.keymap.set("n", "<leader>gd", ":Gvdiffsplit<CR>", { desc = "[G]it [D]iff" })
 		vim.keymap.set("n", "<leader>gb", ":Gblame<CR>", { desc = "[G]it [B]lame" })
+		vim.keymap.set("n", "<leader>gp", ":Git push<CR>", { desc = "[G]it [P]ush" })
+
+		-- Create augroup to namespace this autocmd
+		local group = vim.api.nvim_create_augroup("FugitiveCloseStatusBuffer", { clear = true })
+
+		vim.api.nvim_create_autocmd("FileType", {
+			group = group,
+			pattern = "fugitive",
+			callback = function()
+				vim.keymap.set("n", "q", ":close<CR>", { buffer = true, silent = true })
+			end,
+		})
 	end,
 }
