bd5b93a657b24dff45bb95b59c2961c4adbf760f
SeshuHomePc <seshu.basava70@gmail.com>
2025-03-07 16:20:22 +0100

    Trying to refine nvim-notify fugitive commit message

--- .config/nvim/lua/plugins/fugitive_notify.lua
+++ .config/nvim/lua/plugins/fugitive_notify.lua
@@ -7,13 +7,17 @@ function M.setup()
 			vim.api.nvim_create_autocmd("BufWritePost", {
 				buffer = 0,
 				callback = function()
-					local commit_message = vim.fn.join(vim.fn.readfile(vim.fn.expand("%")), " ")
+					local commit_message = table.concat(vim.fn.readfile(vim.fn.expand("%")), "\n")
 
-					vim.notify(
-						"Committed with message:\n" .. commit_message,
-						vim.log.levels.INFO,
-						{ title = "Git Commit" }
-					)
+					-- Capture git's post-commit summary (like files changed, insertions, deletions)
+					local git_summary = vim.fn.systemlist("git log -1 --stat")
+
+					local combined_message = "Committed with message:\n"
+						.. commit_message
+						.. "\n\n"
+						.. table.concat(git_summary, "\n")
+
+					vim.notify(combined_message, vim.log.levels.INFO, { title = "Git Commit" })
 				end,
 			})
 		end,
