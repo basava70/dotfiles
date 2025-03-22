f80e3f860e92055dab9fde9746df17dc7f444b8c
SeshuHomePc <seshu.basava70@gmail.com>
2025-03-22 23:17:57 +0100

    Added snacks.input, added <leader>gb for git checkout (fugitive), added screen blanking stuff to zshrc.

--- .config/nvim/lua/plugins/fugitive.lua
+++ .config/nvim/lua/plugins/fugitive.lua
@@ -1,11 +1,18 @@
 return {
 	"tpope/vim-fugitive",
 	config = function()
-		-- require("plugins.fugitive_notify").setup() -- Add this line to hook into commits
 		vim.keymap.set("n", "<leader>gs", ":Git<CR>", { desc = "[G]it [S]tatus" })
 		vim.keymap.set("n", "<leader>gd", ":Gvdiffsplit<CR>", { desc = "[G]it [D]iff" })
 		vim.keymap.set("n", "<leader>gb", ":Gblame<CR>", { desc = "[G]it [B]lame" })
 		vim.keymap.set("n", "<leader>gp", ":Git push<CR>", { desc = "[G]it [P]ush" })
+
+		vim.keymap.set("n", "<leader>gb", function()
+			vim.ui.input({ prompt = "New branch name: " }, function(branch)
+				if branch ~= nil and branch ~= "" then
+					vim.cmd("Git checkout -b " .. branch)
+				end
+			end)
+		end, { desc = "[G]it checkout -[b] (new branch)" })
 
 		-- Create augroup to namespace this autocmd
 		local group = vim.api.nvim_create_augroup("FugitiveCloseStatusBuffer", { clear = true })
---
+++ .config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,12 @@
+return {
+	"folke/snacks.nvim",
+	event = "VeryLazy",
+	opts = {
+		-- optional: customize only the input UI
+		input = {
+			border = "rounded",
+			winblend = 10,
+			relative = "editor",
+		},
+	},
+}
