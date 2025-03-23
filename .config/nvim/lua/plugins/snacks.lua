0b3163b8317e4e411b7f9830bd17e000a81b1488
SeshuHomePc <seshu.basava70@gmail.com>
2025-03-23 21:58:06 +0100

    Removed lsp progress from snacks.

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -18,19 +18,5 @@ return {
 				kitty_method = "notify",
 			},
 		})
-
-		-- Basic LSP progress using snacks
-		vim.api.nvim_create_autocmd("LspProgress", {
-			---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
-			callback = function(ev)
-				local spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
-				vim.notify(vim.lsp.status(), vim.log.levels.INFO, {
-					title = "LSP Progress",
-					icon = ev.data.params.value.kind == "end" and ""
-						or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1],
-					replace = "lsp_progress",
-				})
-			end,
-		})
 	end,
 }
