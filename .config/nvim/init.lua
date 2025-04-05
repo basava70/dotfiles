fa58da2331778e0d698adac4f7834b671ac8df1d
SeshuHomePc <seshu.basava70@gmail.com>
2025-04-06 01:36:56 +0200

    Removed fugitive_notifications from init.lua.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -99,4 +99,4 @@ require("lazy").setup({
 	-- The line beneath this is called `modeline`. See `:help modeline`
 	-- vim: ts=2 sts=2 sw=2 et
 })
-require("plugins.fugitive_notifications")
+-- require("plugins.fugitive_notifications")
