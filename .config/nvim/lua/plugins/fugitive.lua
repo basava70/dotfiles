e6a202efbaeaa015b5c052bd642bd2855f1b4deb
SeshuHomePc <seshu.basava70@gmail.com>
2025-04-06 01:46:28 +0200

    Cleaned fugitive.

--- .config/nvim/lua/plugins/fugitive.lua
+++ .config/nvim/lua/plugins/fugitive.lua
@@ -123,25 +123,25 @@ return {
 		})
 
 		-- Show snacks notification after Git commit
-		vim.api.nvim_create_autocmd("User", {
-			pattern = "FugitiveCommitPost",
-			callback = function()
-				local notifier = require("snacks.notifier")
-
-				vim.defer_fn(function()
-					local handle = io.popen("git log -1 --pretty=format:[%h] %s")
-					local output = handle and handle:read("*a") or "Git commit complete"
-					if handle then
-						handle:close()
-					end
-
-					notifier.notify(output, "info", {
-						title = "Git Commit",
-						icon = "",
-						timeout = 10000,
-					})
-				end, 10000)
-			end,
-		})
+		-- vim.api.nvim_create_autocmd("User", {
+		-- 	pattern = "FugitiveCommitPost",
+		-- 	callback = function()
+		-- 		local notifier = require("snacks.notifier")
+		--
+		-- 		vim.defer_fn(function()
+		-- 			local handle = io.popen("git log -1 --pretty=format:[%h] %s")
+		-- 			local output = handle and handle:read("*a") or "Git commit complete"
+		-- 			if handle then
+		-- 				handle:close()
+		-- 			end
+		--
+		-- 			notifier.notify(output, "info", {
+		-- 				title = "Git Commit",
+		-- 				icon = "",
+		-- 				timeout = 10000,
+		-- 			})
+		-- 		end, 10000)
+		-- 	end,
+		-- })
 	end,
 }
