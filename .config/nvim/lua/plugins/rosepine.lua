3772956f73d2a40d19abe406686ebaf61f0584ec
SeshuHomePc <seshu.basava70@gmail.com>
2025-06-04 23:29:09 +0200

    Added nightfox and adjusted rosepine rose color to a smoother one

--- .config/nvim/lua/plugins/rosepine.lua
+++ .config/nvim/lua/plugins/rosepine.lua
@@ -9,18 +9,20 @@ return {
 				["@keyword.conditional"] = { italic = true }, -- if, else
 				["@keyword.repeat"] = { italic = true }, -- for, while
 				["@keyword.return"] = { italic = true }, -- return
-				["@keyword.type"] = {
-					italic = true,
-					fg = "#eb6f92", -- custom color for 'auto', 'const', etc.
-				},
 				["@type.builtin"] = { italic = true }, -- int, float, etc.
 				["@keyword.operator"] = { italic = true }, -- new, delete, etc.
 				["@preproc"] = { italic = true }, -- #define, #include, etc.
 				["@comment"] = { italic = true },
 				["@comment.documentation"] = { italic = true },
 				-- ["@comment.twoline"] = { italic = true },
-				["@comment.tagged"] = { fg = "#eb6f92" },
-				-- ["@string"] = { italic = true },
+				-- ["@comment.tagged"] = { fg = "#eb6f92" },
+				["@string"] = { italic = true },
+			},
+			palette = {
+				main = {
+					rose = "#eaaaA8",
+					-- rose = "#ea9a97",
+				},
 			},
 		})
 		vim.cmd("colorscheme rose-pine")
