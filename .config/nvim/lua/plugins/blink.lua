a4f017b6d06796dfd236191b6f2a3e7345e7aa58
SeshuHomePc <seshu.basava70@gmail.com>
2025-06-18 18:03:28 +0200

    Added keymaps for scroll up and down for documentation. Disabled flash.nvim

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -25,6 +25,8 @@ return {
         ["<C-k>"] = { "select_prev", "fallback_to_mappings" },
         ["<C-j>"] = { "select_next", "fallback_to_mappings" },
         ["<C-s>"] = { "show_signature", "hide_signature", "fallback" },
+        ["<C-u>"] = { "scroll_documentation_up", "fallback" },
+        ["<C-d>"] = { "scroll_documentation_down", "fallback" },
       },
       cmdline = {
         keymap = {
