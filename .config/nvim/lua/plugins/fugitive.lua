77251716fa1de78ed9f349ebf64bf725bae7178b
SeshuHomePc <seshu.basava70@gmail.com>
2025-06-23 07:57:15 +0200

    Added git log key maps in keymaps.lua instead of fugitive. changed blink tab and shift tab functions

--- .config/nvim/lua/plugins/fugitive.lua
+++ .config/nvim/lua/plugins/fugitive.lua
@@ -7,49 +7,6 @@ return {
     vim.keymap.set("n", "<leader>gd", ":Gvdiffsplit<CR>", { desc = "[G]it [D]iff" })
     vim.keymap.set("n", "<leader>gB", ":Gblame<CR>", { desc = "[G]it [B]lame" })
 
-    -- Fancy git log
-    vim.keymap.set("n", "<leader>gl", function()
-      local width = math.floor(vim.o.columns * 0.9)
-      local height = math.floor(vim.o.lines * 0.85)
-      local row = math.floor((vim.o.lines - height) / 2)
-      local col = math.floor((vim.o.columns - width) / 2)
-
-      local buf = vim.api.nvim_create_buf(false, true)
-      local win = vim.api.nvim_open_win(buf, true, {
-        relative = "editor",
-        row = row,
-        col = col,
-        width = width,
-        height = height,
-        style = "minimal",
-        border = "rounded",
-        title = "Git Log",
-        title_pos = "center",
-      })
-      -- Get how many lines the log will produce
-      local log_lines = vim.fn.systemlist(
-        [[git log --graph --all --color=always --pretty=format:'%C(auto)%h%Creset %C(yellow)%d%Creset %s %Cgreen(%ar)%Creset %C(bold blue)%an%Creset']]
-      )
-      if vim.v.shell_error ~= 0 then
-        vim.api.nvim_buf_set_lines(buf, 0, -1, false, { "Git log failed." })
-        return
-      end
-      local should_insert = #log_lines >= height - 4
-      vim.api.nvim_buf_call(buf, function()
-        vim.fn.termopen({
-          "sh",
-          "-c",
-          [[git log --graph --all --color=always --pretty=format:'%C(auto)%h%Creset %C(yellow)%d%Creset %s %Cgreen(%ar)%Creset %C(bold blue)%an%Creset'; sleep 1000d]],
-        })
-      end)
-      if should_insert then
-        vim.cmd("startinsert")
-      end
-      -- Add this after termopen block
-      vim.keymap.set({ "n", "t" }, "q", "<C-\\><C-n>:close<CR>", { buffer = buf, silent = true })
-      vim.keymap.set("t", "<Esc>", "<C-\\><C-n>:close<CR>", { buffer = buf, silent = true })
-    end, { desc = "[G]it [L]og (smart terminal)" })
-
     -- Push branch to origin (works even if its a new branch)
     vim.keymap.set("n", "<leader>gp", function()
       local handle = io.popen("git rev-parse --abbrev-ref HEAD")
