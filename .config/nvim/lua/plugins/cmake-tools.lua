8a5a1752a517119ec6c92594190c2221873b357d
SeshuHomePc <seshu.basava70@gmail.com>
2025-11-21 00:29:43 +0100

    Added -Xcompiler=-fPIC and removed pause on cmake_executors

--- .config/nvim/lua/plugins/cmake-tools.lua
+++ .config/nvim/lua/plugins/cmake-tools.lua
@@ -2,12 +2,5 @@ return {
   "Civitasv/cmake-tools.nvim",
   opts = {
     filetypes = { "c", "cpp", "cuda", "objc", "objcpp", "cmake" },
-    cmake_executor = {
-      default_opts = {
-        quickfix = {
-          auto_close_when_success = false,
-        },
-      },
-    },
   },
 }
