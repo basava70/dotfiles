170fdce3cae1a45536c3caddcddfc91d8f96fd81
SeshuHomePc <seshu.basava70@gmail.com>
2025-12-01 13:27:22 +0100

    Changed back to RosePine

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,57 +1,96 @@
 -- lua/plugins/rose-pine.lua
 return {
-  {
-    "folke/tokyonight.nvim",
-    lazy = true,
-    opts = { style = "night" },
-  },
-  -- "rose-pine/neovim",
-  -- name = "rose-pine",
-  -- config = function()
-  --   require("rose-pine").setup({
-  --     disable_italics = true,
-  --     highlight_groups = {
-  --       ["@keyword.conditional"] = { italic = true }, -- if, else
-  --       ["@keyword.repeat"] = { italic = true }, -- for, while
-  --       ["@keyword.return"] = { italic = true }, -- return
-  --       ["@type.builtin"] = { italic = true }, -- int, float, etc.
-  --       ["@keyword.operator"] = { italic = true }, -- new, delete, etc.
-  --       ["@preproc"] = { italic = true }, -- #define, #include, etc.
-  --       ["@comment"] = { italic = true },
-  --       ["@comment.documentation"] = { italic = true },
-  --       ["@comment.tagged"] = { fg = "#eb6f92" },
-  --       ["@string"] = { italic = true },
-  --     },
-  --     palette = {
-  --       main = {
-  --         rose = "#eaaaA8",
-  --         -- rose = "#ea9a97",
+  -- {
+  --   "catppuccin/nvim",
+  --   lazy = true,
+  --   name = "catppuccin",
+  --   opts = {
+  --     lsp_styles = {
+  --       underlines = {
+  --         errors = { "undercurl" },
+  --         hints = { "undercurl" },
+  --         warnings = { "undercurl" },
+  --         information = { "undercurl" },
   --       },
   --     },
-  --     styles = {
-  --       transparency = true,
+  --     integrations = {
+  --       aerial = true,
+  --       alpha = true,
+  --       cmp = true,
+  --       dashboard = true,
+  --       flash = true,
+  --       fzf = true,
+  --       grug_far = true,
+  --       gitsigns = true,
+  --       headlines = true,
+  --       illuminate = true,
+  --       indent_blankline = { enabled = true },
+  --       leap = true,
+  --       lsp_trouble = true,
+  --       mason = true,
+  --       mini = true,
+  --       navic = { enabled = true, custom_bg = "lualine" },
+  --       neotest = true,
+  --       neotree = true,
+  --       noice = true,
+  --       notify = true,
+  --       snacks = true,
+  --       telescope = true,
+  --       treesitter_context = true,
+  --       which_key = true,
   --     },
-  --   })
-  --   vim.cmd("colorscheme rose-pine")
-  --   -- 		main = {
-  --   -- 	_nc = "#16141f",
-  --   -- 	base = "#191724",
-  --   -- 	surface = "#1f1d2e",
-  --   -- 	overlay = "#26233a",
-  --   -- 	muted = "#6e6a86",
-  --   -- 	subtle = "#908caa",
-  --   -- 	text = "#e0def4",
-  --   -- 	love = "#eb6f92",
-  --   -- 	gold = "#f6c177",
-  --   -- 	rose = "#ebbcba",
-  --   -- 	pine = "#31748f",
-  --   -- 	foam = "#9ccfd8",
-  --   -- 	iris = "#c4a7e7",
-  --   -- 	leaf = "#95b1ac",
-  --   -- 	highlight_low = "#21202e",
-  --   -- 	highlight_med = "#403d52",
-  --   -- 	highlight_high = "#524f67",
-  --   -- 	none = "NONE",
-  --   -- },
-  -- end,
+  --   },
+  --   specs = {
+  --     {
+  --       "akinsho/bufferline.nvim",
+  --       optional = true,
+  --       opts = function(_, opts)
+  --         if (vim.g.colors_name or ""):find("catppuccin") then
+  --           opts.highlights = require("catppuccin.special.bufferline").get_theme()
+  --         end
+  --       end,
+  --     },
+  --   },
+  -- },
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   lazy = true,
+  --   opts = { style = "night" },
+  -- },
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    config = function()
+      require("rose-pine").setup({
+        disable_italics = true,
+        highlight_groups = {
+          ["@keyword.conditional"] = { italic = true }, -- if, else
+          ["@keyword.repeat"] = { italic = true }, -- for, while
+          ["@keyword.return"] = { italic = true }, -- return
+          ["@type.builtin"] = { italic = true }, -- int, float, etc.
+          ["@keyword.operator"] = { italic = true }, -- new, delete, etc.
+          ["@preproc"] = { italic = true }, -- #define, #include, etc.
+          ["@comment"] = { italic = true },
+          ["@comment.documentation"] = { italic = true },
+          -- ["@comment.tagged"] = { fg = "#eb6f92" },
+          ["@string"] = { italic = true },
+        },
+        palette = {
+          main = {
+            rose = "#eaaaA8",
+            -- rose = "#ea9a97",
+          },
+        },
+        styles = {
+          transparency = true,
+        },
+      })
+    end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "rose-pine",
+    },
+  },
 }
