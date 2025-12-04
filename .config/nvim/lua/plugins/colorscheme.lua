b5dffec67544369f5946ad247a04b87e02a8ddd7
SeshuHomePc <seshu.basava70@gmail.com>
2025-12-04 14:09:45 +0100

    Updated rose-pine

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -61,30 +61,30 @@ return {
     "rose-pine/neovim",
     name = "rose-pine",
     config = function()
-      require("rose-pine").setup({
-        disable_italics = true,
-        highlight_groups = {
-          ["@keyword.conditional"] = { italic = true }, -- if, else
-          ["@keyword.repeat"] = { italic = true }, -- for, while
-          ["@keyword.return"] = { italic = true }, -- return
-          ["@type.builtin"] = { italic = true }, -- int, float, etc.
-          ["@keyword.operator"] = { italic = true }, -- new, delete, etc.
-          ["@preproc"] = { italic = true }, -- #define, #include, etc.
-          ["@comment"] = { italic = true },
-          ["@comment.documentation"] = { italic = true },
-          -- ["@comment.tagged"] = { fg = "#eb6f92" },
-          ["@string"] = { italic = true },
-        },
-        palette = {
-          main = {
-            rose = "#eaaaA8",
-            -- rose = "#ea9a97",
-          },
-        },
-        styles = {
-          transparency = true,
-        },
-      })
+      -- require("rose-pine").setup({
+      --   disable_italics = true,
+      --   highlight_groups = {
+      --     ["@keyword.conditional"] = { italic = true }, -- if, else
+      --     ["@keyword.repeat"] = { italic = true }, -- for, while
+      --     ["@keyword.return"] = { italic = true }, -- return
+      --     ["@type.builtin"] = { italic = true }, -- int, float, etc.
+      --     ["@keyword.operator"] = { italic = true }, -- new, delete, etc.
+      --     ["@preproc"] = { italic = true }, -- #define, #include, etc.
+      --     ["@comment"] = { italic = true },
+      --     ["@comment.documentation"] = { italic = true },
+      --     -- ["@comment.tagged"] = { fg = "#eb6f92" },
+      --     ["@string"] = { italic = true },
+      --   },
+      --   palette = {
+      --     main = {
+      --       rose = "#eaaaA8",
+      --       -- rose = "#ea9a97",
+      --     },
+      --   },
+      --   styles = {
+      --     transparency = true,
+      --   },
+      -- })
     end,
   },
   {
