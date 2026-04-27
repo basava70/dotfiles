9274715a4cba418079e16de8e77ec5c937b81c52
SeshuHomePc <seshu.basava70@gmail.com>
2026-04-27 09:39:18 +0200

    Changed to FiraCode + Radon setup in both Kitty and Ghostty

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,4 +1,4 @@
--- lua/plugins/rose-pine.lua
+-- lua/plugins/colorscheme.lua
 return {
   {
     "folke/tokyonight.nvim",
@@ -8,15 +8,33 @@ return {
   {
     "rose-pine/neovim",
     name = "rose-pine",
+    config = function()
+      require("rose-pine").setup({
+        disable_italics = true,
+
+        highlight_groups = {
+          ["Comment"] = { italic = true },
+          ["@comment"] = { italic = true },
+          ["@comment.documentation"] = { italic = true },
+          ["@lsp.type.comment"] = { italic = true },
+          ["@lsp.type.comment.cpp"] = { italic = true },
+        },
+
+        palette = {
+          main = {
+            rose = "#eaaaA8",
+          },
+        },
+      })
+    end,
   },
-  { "EdenEast/nightfox.nvim" }, -- lazy
+  { "EdenEast/nightfox.nvim" },
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   {
     "LazyVim/LazyVim",
     import = "lazyvim.plugins",
     opts = {
       colorscheme = "rose-pine",
-      -- colorscheme = "catppuccin-mocha",
     },
   },
 }
