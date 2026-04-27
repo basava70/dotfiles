db23acd7132b7ea92c41e95963909fde7be528e3
SeshuHomePc <seshu.basava70@gmail.com>
2026-04-27 20:52:24 +0200

    Terminal now default opens on the right

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -2,6 +2,11 @@ return {
   {
     "folke/snacks.nvim",
     opts = {
+      terminal = {
+        win = {
+          position = "right",
+        },
+      },
       picker = {
         win = {
           input = {
